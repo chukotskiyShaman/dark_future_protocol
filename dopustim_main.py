7bb4efee4c33766b92aedd5cd023980a2e7823bc
Porweks <svjatking42@mail.ru>
2024-05-21 20:53:15 +0700

    sone additions in ch3

--- dopustim_main.py
+++ dopustim_main.py
@@ -510,7 +510,7 @@ class MainWindow(QMainWindow):
                 
     def opposition(self, i):
         path=''
-        if not i == 2:
+        if not (i == 2 or i == 3):
             self.hide_buttons(self.choises[0:2])
         self.label.setText('')
 
@@ -524,10 +524,20 @@ class MainWindow(QMainWindow):
             return
 
         if i ==2:
+            self.menu_buttons[0].disconnect()
+            self.menu_buttons[0].clicked.connect(lambda _:self.opposition(3))
             with open('./data/chapter2/help_partisans.txt', 'r', encoding='utf-8') as file:
                 self.text=file.read()
             self.print_text(self.label)
+            self.menu_buttons[0].show()
             return
+        
+        if i == 3:
+            path = './data/chapter3/local_office_variants.txt'
+            with open('./data/chapter3/local_office.txt', 'r', encoding='utf-8') as file:
+                self.text=file.read()
+            self.print_text(self.label,self.choises[0:2], self.attack)
+            
         
         if i == 1:
             path = './data/chapter2/decline_variants.txt'
@@ -540,6 +550,44 @@ class MainWindow(QMainWindow):
                 if not (string == '\n'):
                     self.choises[i].setText(string)
                     self.choises[i].setGeometry(1470,600+i*40,450,40)
+
+    def attack(self,i):
+        path=''
+        self.hide_buttons(self.choises[0:2])
+        self.label.setText('')
+        if i == 0:
+            path = './data/chapter3/daylight_variants.txt'
+            with open('./data/chapter3/daylight.txt', 'r', encoding='utf-8') as file:
+                self.text=file.read()
+        self.print_text(self.label,self.choises[0:2], self.day)
+
+        if i == 1:
+            path = './data/chapter3/stels_variants.txt'
+            with open('./data/chapter3/stels.txt', 'r', encoding='utf-8') as file:
+                self.text=file.read()
+        self.print_text(self.label,self.choises[0:2], self.night)
+
+        with open(path,'r',encoding = "utf-8") as file:
+            for i,string in enumerate(file): 
+                if not (string == '\n'):
+                    self.choises[i].setText(string)
+                    self.choises[i].setGeometry(1470,600+i*40,450,40)
+
+    def day(self,i):
+        return
+    
+    def night(self,i):
+        path=''
+        self.hide_buttons(self.choises[0:2])
+        self.label.setText('')
+        if i ==0:
+            with open('./data/chapter3/look_around.txt', 'r', encoding='utf-8') as file:
+                self.text=file.read()
+        if i == 1:
+            with open('./data/chapter3/use_terminal_stels.txt', 'r', encoding='utf-8') as file:
+                self.text=file.read()
+        self.print_text(self.label)
+
 
     def decline(self,i):
         path=''
